32f1c0a66eaedc25b82cc0617b34d4891768e0bf
ticktakclock <ticchettio.di.un.orologio@gmail.com>
2020-06-28 14:17:44 +0900

    Add content data

--- app/src/main/java/com/github/ticktakclock/sharedelementsample/Photo.kt
+++ app/src/main/java/com/github/ticktakclock/sharedelementsample/Photo.kt
@@ -36,23 +36,48 @@ data class Photo(val id: Int, val title: String, val imageUrl: String) {
             ),
             Photo(
                 6,
-                "いらすと",
-                "https://1.bp.blogspot.com/-hq2NkdVdvYg/XuHCjmgH9eI/AAAAAAABZdI/wh1lUWAgZg84CFxP9aX3cqvUn1fuRIVsACNcBGAsYHQ/s1600/tablet_family.png"
+                "かに玉",
+                "https://1.bp.blogspot.com/-YDGkzYON_Vg/XmHk2ovVFwI/AAAAAAABXvw/xx3Gjdo4Z54phHjNgcMkevyHeXuRGZ0SgCNcBGAsYHQ/s180-c/food_kanitama.png"
             ),
             Photo(
                 7,
-                "いらすと",
-                "https://1.bp.blogspot.com/-ZG_36wOnNWo/Xtt6kiYgQkI/AAAAAAABZR8/TOPFp_CDUWUTA45IS-Tv3DEShmm6OxGdACNcBGAsYHQ/s400/nyuin_online_menkai_woman.png"
+                "宇宙食",
+                "https://1.bp.blogspot.com/-T3ZsIsLkDVs/XkZdPFKa4uI/AAAAAAABXVY/trSE9300P6YX388N5-Al8m0gIZFRHuivACNcBGAsYHQ/s180-c/space_food.png"
             ),
             Photo(
                 8,
-                "いらすと",
-                "https://1.bp.blogspot.com/-hq2NkdVdvYg/XuHCjmgH9eI/AAAAAAABZdI/wh1lUWAgZg84CFxP9aX3cqvUn1fuRIVsACNcBGAsYHQ/s1600/tablet_family.png"
+                "お好み焼き",
+                "https://1.bp.blogspot.com/-MsI-cwG9LmY/Xbo7IE3judI/AAAAAAABVzA/qJKp2sMiCckuEn3KP1yzX79no-fWyq8DgCNcBGAsYHQ/s180-c/food_okonomiyaki_kyabetsuyaki_decoration.png"
             ),
             Photo(
                 9,
-                "いらすと",
-                "https://1.bp.blogspot.com/-ZG_36wOnNWo/Xtt6kiYgQkI/AAAAAAABZR8/TOPFp_CDUWUTA45IS-Tv3DEShmm6OxGdACNcBGAsYHQ/s400/nyuin_online_menkai_woman.png"
+                "餃子",
+                "https://1.bp.blogspot.com/-UbN0yy2MMrM/XaKa5_0WWUI/AAAAAAABVj8/-YGIDDkzgPUkGRe9odTWisMXeIZBTK9wgCNcBGAsYHQ/s180-c/food_curryrice_white.png"
+            ),
+            Photo(
+                10,
+                "カレーライス",
+                "https://1.bp.blogspot.com/-UbN0yy2MMrM/XaKa5_0WWUI/AAAAAAABVj8/-YGIDDkzgPUkGRe9odTWisMXeIZBTK9wgCNcBGAsYHQ/s180-c/food_curryrice_white.png"
+            ),
+            Photo(
+                11,
+                "エビフライ",
+                "https://1.bp.blogspot.com/-N4wY9yIahmw/XVKf6aSOBZI/AAAAAAABUFs/ENGPYG_Cq7kP5TWvamiXS5Ak_Q5NFsyuwCLcBGAs/s180-c/food_ebi_fry_set.png"
+            ),
+            Photo(
+                12,
+                "鍋",
+                "https://1.bp.blogspot.com/-jXIyJNC8ofg/XTPoHxzHSPI/AAAAAAABTwQ/pZCjTsWrx_UZ_7q5kvMHS1KHGdNAveRDACLcBGAs/s180-c/food_nabe_mizutaki.png"
+            ),
+            Photo(
+                13,
+                "ポテトフライ",
+                "https://1.bp.blogspot.com/-U5tiBAaVncQ/XQjuEWqlAmI/AAAAAAABTPI/c-nmr6K9WmwW7vOYBYIU5q_1PtdhuVFQgCLcBGAs/s180-c/food_fried_potato_dish.png"
+            ),
+            Photo(
+                14,
+                "ポットパイ",
+                "https://4.bp.blogspot.com/-hb0SFii_eZQ/XGjx4QtFWNI/AAAAAAABRck/-wzYshwz0KsRngk4eUEexgsJXqk2jT2QACLcBGAs/s180-c/food_soup_pie_dutumi_potpie.png"
             )
         )
 
